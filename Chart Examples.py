1f8f44f0c3814c45124dcf39f8a4a56ac25c7276
easymoney08 <kana8mkm@gmail.com>
2023-09-06 17:39:56 +0600

    Создание основы программы

---
+++ Chart Examples.py
@@ -0,0 +1,12 @@
+#Примеры псотроения графиков
+
+import tkinter as tk
+
+#Создание главного окна
+window = tk.Tk()
+window.geometry("450x450")
+window.title("Примеры построения графиков")
+
+
+# Запуск цикла mainloop
+window.mainloop()
